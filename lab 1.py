fbf3de0d611d0715d4c74a3f3e5a6e93bf7b69ec
emilycatanzariti <ejcatanzariti@loyola.edu>
2021-09-16 11:26:18 -0400

    wrote code

--- lab 1.py
+++ lab 1.py
@@ -2,5 +2,17 @@
 # Course: CS151, Dr. Rajeev
 # Date: 9/16/2021
 # Lab number 1
-# Program Inputs
-# Program Outputs
+# Program Inputs: [number of milliliters]
+# Program Outputs: [output 1 teaspoons, output 2 tablespoon]
+
+# Ask for input of mL
+mL = input("Enter number of mL")
+mL = int(mL)
+
+# Code for calculating first output
+tsp = (mL * 5)
+print(tsp)
+
+# Code for calculating second output
+tbsp = (tsp * 3)
+print(tbsp)
